66c654b4bfdea29666d342ee6a124726f84a8d95
tobifrank <tobifrank38@gmail.com>
2013-10-18 22:05:08 +0200

    Fix time output

--- lib/runner.rb
+++ lib/runner.rb
@@ -12,14 +12,14 @@ module Sunweather
 			@geo = (ARGV[0] ? Geo.new(ARGV[0]) : Geo.new)
 			@sun = Sun.new(@geo.lat, @geo.lng)
 			@weather = Weather.new(@geo.lat, @geo.lng)
-			puts "Dawn from #{hours_minutes(@sun.start_of_dawn)} to #{hours_minutes(@sun.sunrise)}"
-			puts "Dusk from #{hours_minutes(@sun.sunset)} to #{hours_minutes(@sun.end_of_dusk)}"
-			puts "Temperature: #{@weather.temperature}°C, feels like #{@weather.feels_like}°C"
-			puts "#{@weather.conditions} sky, #{@weather.wind_speed.downcase} wind from #{@weather.wind_direction}."
+			puts "Dawn from #{hours_minutes(@sun.start_of_dawn)} to #{hours_minutes(@sun.sunrise)}."
+			puts "Dusk from #{hours_minutes(@sun.sunset)} to #{hours_minutes(@sun.end_of_dusk)}."
+			puts "Temperature: #{@weather.temperature}°C, feels like #{@weather.feels_like}°C."
+			puts "Weather: #{@weather.conditions}, Winds #{@weather.wind_speed.downcase}, direction #{@weather.wind_direction}."
 		end
 
 		def hours_minutes time
-			"#{time.hour}:#{time.min+time.sec/30}"
+			%-#{time.hour}:#{"%02i" % (time.min+time.sec/30)}-
 		end
 	end
 end
